8bca1aef249e35d1ffda932b5f4eea79fad85cce
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:43:10 -0500

    add car

--- src/Car.java
+++ src/Car.java
@@ -1,12 +1,18 @@
 public class Car {
 
+<<<<<<< HEAD
     private boolean wheels;
     private int cylinders;
+=======
+    private final boolean wheels;
+    private final int cylinders;
+>>>>>>> c2c96c2 (add car class)
 
     public Car(boolean wheels, int cylinders) {
         this.wheels = wheels;
         this.cylinders = cylinders;
     }
+<<<<<<< HEAD
 
     public boolean isWheels() {
         return wheels;
@@ -28,4 +34,6 @@ public class Car {
     public String toString() {
         return "Wheels: " + wheels + " cylinders: " + cylinders + "\n";
     }
+=======
+>>>>>>> c2c96c2 (add car class)
 }
