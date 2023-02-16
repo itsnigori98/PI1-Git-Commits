ce559fcb829ce5e6cfec4e52af355708ca54ad73
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:47:23 -0500

    feat: add readme.md for project description
    - Added for-loop iteration for car list
    - Implemented toString() and print functionality

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,7 @@
 public class Car {
 
-    private final boolean wheels;
-    private final int cylinders;
+    private boolean wheels;
+    private int cylinders;
 
     public Car(boolean wheels, int cylinders) {
         this.wheels = wheels;
@@ -12,7 +12,20 @@ public class Car {
         return wheels;
     }
 
+    public void setWheels(boolean wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+
     public int getCylinders() {
         return cylinders;
+    }
+
+    @Override
+    public String toString() {
+        return "Wheels: " + wheels + "cylinders: " + cylinders + "\n";
     }
 }
